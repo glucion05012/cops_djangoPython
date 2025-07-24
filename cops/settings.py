62aeca80b0ab0b11d441020ed3b6bad1acd0c528
Giddel Lucion <giddel.lucion@yahoo.com>
2025-07-24 14:50:59 +0800

    generate pdf

--- cops/settings.py
+++ cops/settings.py
@@ -143,6 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'crs', 'static'),
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"  # or another folder
 
 # Default primary key field type
